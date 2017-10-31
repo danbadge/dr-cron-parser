a6e0609e411cc986b3aa47a39384de53698f8ab3
Daniel Watts <danbadge@gmail.com>
2017-10-31 22:29:49 +0000

    and we're using the parser :)

--- lib/command_runner.rb
+++ lib/command_runner.rb
@@ -12,7 +12,7 @@ class CommandRunner
 
     parser = CronParser.new(args[0])
 
-    logger.info("\nminute        0
+    logger.info("\nminute        #{parser.minute}
 hour          0
 day of month  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
 month         1 2 3 4 5 6 7 8 9 10 11 12
